36a17c3a469f31de1bb681f34806bf10f9c6189f
unknown <estudiante@LAB0245.SERVERINT.LOCAL>
2017-03-07 12:28:30 -0600

    examen5tobaco

---
+++ script.js
@@ -0,0 +1,11 @@
+var BacoA = angular.modular("BacoA";[]);
+BacoA.controller("mensaje",function($scope) {
+	$scope.texto = "hola"
+	}) 
+
+	BacoA.controller("nombre",function($scope) {
+		// body...
+
+	$scope.nombre = "maria"
+	// body...
+}) 
